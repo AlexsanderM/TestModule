2ab554bb81030552c0d138424fd3c8c50b601ff4
Malov <malov.alexsander@yandex.ru>
2018-08-11 20:26:44 +0300

    add metod maxArray

---
+++ TestModule/TestModule/ArrayMan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestModule
+{
+    public class ArrayMan
+    {
+        public static int FindMax(int[] a) {
+            int max = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
