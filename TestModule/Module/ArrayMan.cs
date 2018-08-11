ba4dbc7001c9a4fbfbcb401436a5b51073fabb2b
Malov <malov.alexsander@yandex.ru>
2018-08-11 20:52:23 +0300

    test array on max

---
+++ TestModule/Module/ArrayMan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module
+{
+    public class ArrayMan
+    {
+        public int FindMax(int[] a) {
+            int max = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ TestModule/TestModule.Test/ArrayManTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Module;
+
+namespace TestModule.Test
+{
+    [TestClass]
+    public class ArrayManTests
+    {
+        ArrayMan arrMan = new ArrayMan();
+
+        [TestMethod]
+        public void FindMax_PositivElement_Test()
+        {
+            
+            int[] a = { -4, 3, 22, -43, 44, 3 };
+            int expected = 44;
+
+            int actual = arrMan.FindMax(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
